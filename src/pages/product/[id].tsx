9369cf408c85f1fa96e92d2e6058a37cd35b90c8
YuriODantas <iruy3343@gmail.com>
2024-01-10 21:33:13 -0300

    First Commit

---
+++ src/pages/product/[id].tsx
@@ -0,0 +1,9 @@
+import { useRouter } from 'next/router'
+import React from 'react'
+
+export default function Product() {
+  const { query } = useRouter()
+  return (
+    <div>Product: {query.id}</div>
+  )
+}
