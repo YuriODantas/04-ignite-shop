ff7922ffd8ea73f9aec82a429e477651fa516dcf
YuriODantas <iruy3343@gmail.com>
2024-01-18 20:57:25 -0300

    feat: create style product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,24 @@
 import { useRouter } from 'next/router'
 import React from 'react'
+import { ImageContainer, ProductContainer, ProductDetails } from '../../styles/pages/product'
 
 export default function Product() {
   const { query } = useRouter()
+
   return (
-    <div>Product: {query.id}</div>
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79.90</span>
+
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae, fugiat ipsam? Iste mollitia unde aut sit optio. Velit dolor magni ea nemo, rem iusto non, excepturi tempore nisi deserunt optio.</p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
